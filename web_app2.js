f6341e6346f62aa91ec53979178cd0ce07d87dc0
OsinachiAtama <osinachiatama@gmail.com>
2025-04-22 14:48:26 -0400

    Create web_app2.js

---
+++ web_app2.js
@@ -0,0 +1,110 @@
+//Interacting with page 
+var http = require('http'); 
+var url = require('url'); 
+var fs = require('fs');
+
+
+//Create Server
+http.createServer(function(req, res) { 
+    //Tell server the content of the file
+    urlObj = url.parse(req.url, true)
+    path = urlObj.pathname;
+    var query = urlObj.query; //Will get json looking query string
+
+    if (path == "/") { 
+        file = __dirname + "/home.html"; 
+        fs.readFile(file, function(err, home) { 
+            res.writeHead(200, {'Content-Type': 'text/html'}); 
+            res.write(home); 
+            res.end(); 
+        })
+        
+    } else if (path == "/process") {
+
+        res.writeHead(200, {'Content-Type': 'text/html'}); 
+        res.write(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Process</title>
+                <link rel="stylesheet" href="/style.css" />
+            </head>
+            <body>
+                Processing... </br>
+            </body>
+            </html>`);
+        
+        //Access GET parameters 
+
+        var word = query.word; 
+        var stk_or_comp = query.stk_or_comp; 
+            
+        console.log(word, stk_or_comp);
+        if (stk_or_comp == "comp_name") { 
+            query = {"companyName" : word}; 
+        } else if (stk_or_comp == "stk_sym") { 
+            query = {"stkTkr" : word};
+        }
+
+
+        //Connect to Mongodb 
+        const {MongoClient} = require('mongodb');
+        const connStr= "mongodb+srv://Osinachi:lordofhosts@cluster0.enps8.mongodb.net/"
+        const client = new MongoClient(connStr);
+     
+        async function run(){ 
+            try { 
+                await client.connect();
+                var dbo = client.db("Stock");
+                var collection = dbo.collection('PublicCompanies');           
+                var results = await collection.find(query).toArray(); 
+                if (results.length == 0) { 
+                    console.log("Sorry, your input does not match any of our records."); 
+                    res.write("Sorry, your input does not match any of our records."); 
+
+                } else { 
+                    res.write("<p id = 'doc'>");
+                    console.log("Here are the records that match your input: "); 
+                    res.write("<span>Here are the records that match your input:</span> <br> <br>"); 
+
+                    results.forEach(function(doc) { 
+                       
+                        for (field in doc){  
+                            if (field != "_id") { 
+                                console.log( field, ": ", doc[field]);
+                                res.write(`${field}: ${doc[field]}<br>`);
+                            }
+                        }
+                        
+                    })
+                    res.write("</p>");
+                    console.log("\n");
+                }
+               
+                res.end();
+
+            } catch (dbErr){ 
+                console.log("Database error: " + dbErr);
+                res.end();
+            }
+        }
+        run();
+
+    } else if (path == "/style.css") { 
+        file =  __dirname + "style.css";
+        fs.readFile(file, function (err,  style) { 
+            if (err) { 
+                res.writeHead(404); 
+                res.end("CSS file could not be found"); 
+                return; 
+            }
+            res.writeHead(200, {'Content-Type': 'text/css'} )
+            res.write(style); 
+            res.end();
+        })
+    }
+
+    //Close server
+    // res.end();
+}).listen(8080)
